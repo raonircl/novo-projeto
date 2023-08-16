1af96af15448e1a10f8dac9adcd6da041ac730ae
raonircl <raoni.afi@gmail.com>
2023-08-16 01:34:21 -0300

    feat: method create

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,9 +10,23 @@ class ProductsController < ApplicationController
     @product = Product.new
   end
 
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to @product      
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def set_product
     @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:nome, :price, :amount)
   end
 end
